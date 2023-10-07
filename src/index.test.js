0ebd5278e6cb9766d3b362efda3765805d074bf9
張天慈 <woodychang0611@gmail.com>
2023-10-07 21:57:11 +0800

    clean up

--- src/index.test.js
+++ src/index.test.js
@@ -5,11 +5,9 @@ import Index from './index.js'; // Import your React component
 
 test('renders an h1 element with the text "react hello world!"', () => {
   // Render the component
-  //const { getByText } = render(<HelloWorld/>);
   const { getByText } = render(<Index/>);
   // Use getByText to find the <h1> element with the specified text
   const h1Element = getByText('React Hello World!');
-
   // Assert that the <h1> element with the expected text exists
   expect(h1Element).toBeInTheDocument();
 });
